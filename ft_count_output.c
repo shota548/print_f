439b8ea40c0855cde4edc7a5c183d55081ed9b52
Shota Watanabe <showatan@c2r2s6.42tokyo.jp>
2025-06-21 17:45:24 +0900

    0621_v2

--- ft_count_output.c
+++ ft_count_output.c
@@ -6,7 +6,7 @@
 /*   By: showatan <showatan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 09:23:07 by showatan          #+#    #+#             */
-/*   Updated: 2025/06/21 17:30:06 by showatan         ###   ########.fr       */
+/*   Updated: 2025/06/21 17:45:02 by showatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_count_output(const char	*input, va_list args)
 	{
 		if (*input == '%' && *(input + 1) == '\0')
 			return (-1);
-		else if (*input == '%' && ft_strchr("cspduxX%", *(input + 1)))
+		else if (*input == '%' && ft_strchr("cspdiuxX%", *(input + 1)))
 		{
 			input++;
 			count += ft_treat_something(input, args);
