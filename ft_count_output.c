cb53fb4b74700a9360d3bad08f1edbedc98d754a
Shota Watanabe <showatan@c2r2s6.42tokyo.jp>
2025-06-21 17:32:58 +0900

    0621

--- ft_count_output.c
+++ ft_count_output.c
@@ -6,7 +6,7 @@
 /*   By: showatan <showatan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 09:23:07 by showatan          #+#    #+#             */
-/*   Updated: 2025/06/14 17:26:06 by showatan         ###   ########.fr       */
+/*   Updated: 2025/06/21 17:30:06 by showatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ int	ft_count_output(const char	*input, va_list args)
 	count = 0;
 	while (*input != '\0')
 	{
-		if (*input == '%')
+		if (*input == '%' && *(input + 1) == '\0')
+			return (-1);
+		else if (*input == '%' && ft_strchr("cspduxX%", *(input + 1)))
 		{
 			input++;
 			count += ft_treat_something(input, args);
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: showatan <showatan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 15:45:18 by showatan          #+#    #+#             */
-/*   Updated: 2025/06/14 17:25:41 by showatan         ###   ########.fr       */
+/*   Updated: 2025/06/21 17:30:14 by showatan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_printf(const char	*input, ...)
 
 	num = 0;
 	if (input == NULL)
-		return (0);
+		return (-1);
 	va_start(args, input);
 	num = ft_count_output(input, args);
 	va_end(args);
